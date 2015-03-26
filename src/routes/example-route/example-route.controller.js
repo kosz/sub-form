157b424fed6dc86a2968d81fda4a4aa7ef0c9aaf
Cosmin Ronnin <coszmin@gmail.com>
2015-03-25 20:13:59 -0400

    chore(scaffold): deleted not needed scaffold code

--- src/routes/example-route/example-route.controller.js
+++
@@ -1,8 +0,0 @@
- 
-angular.module('sub-form').controller('example-route', [ 
-function () {
-  var self = this;
-
-  console.log('Started controller example-route'); 
-
-}]);
--- src/routes/main/main.controller.spec.js
+++ src/routes/main/main.controller.spec.js
@@ -1,20 +1,18 @@
 describe('Controller: main', function() {
-  var controller, $rootScope;
-    
+  var $controller, controller, $rootScope;
 
-  beforeEach(module('sub-form')); 
-    beforeEach(inject(function (_$controller_,_$rootScope_) { 
-    
-        $controller = _$controller_;
-        $rootScope = _$rootScope_;
-        
+  beforeEach(module('sub-form'));
+  beforeEach(inject(function (_$controller_,_$rootScope_) {
 
-    controller = $controller('main', { 
-      });
+    $controller = _$controller_;
+    $rootScope = _$rootScope_;
+
+    controller = $controller('main', {
+    });
   }));
 
   it('should get initialized', function() {
     expect(controller).not.toEqual(undefined);
-  }); 
+  });
 
 });
